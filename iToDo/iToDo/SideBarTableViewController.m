6231c5440dcfee65bc8f31bbe22e056346d16977
Bilal <bilal.arslan1993@hotmail.com>
2014-07-29 00:38:45 +0300

    Slide Menu Completed!

--- iToDo/iToDo/SideBarTableViewController.m
+++ iToDo/iToDo/SideBarTableViewController.m
@@ -89,22 +89,21 @@
     return 44;
 }
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if(indexPath.row > 0)
-    {
-        ToDoTableViewController *toDoVC = [[ToDoTableViewController alloc] init];
-        [self presentViewController:toDoVC animated:YES completion:nil];
-    }
-}
+//-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    if(indexPath.row > 0)
+//    {
+//        ToDoTableViewController *toDoVC = [[ToDoTableViewController alloc] init];
+//        [self presentViewController:toDoVC animated:YES completion:nil];
+//    }
+//}
 
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    ToDoTableViewController *ToDoVC = [segue destinationViewController];
-    [self presentViewController:ToDoVC animated:YES completion:nil];    
+    
 }
 
 @end
