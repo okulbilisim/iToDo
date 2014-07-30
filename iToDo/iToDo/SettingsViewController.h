cf131e9ee2216b8899b10f84840508864b4a014e
Bilal <bilal.arslan1993@hotmail.com>
2014-07-30 22:34:07 +0300

    small update
    setting class added.

---
+++ iToDo/iToDo/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  iToDo
+//
+//  Created by Bilal ARSLAN on 30/07/14.
+//  Copyright (c) 2014 Bilal ARSLAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ iToDo/iToDo/SettingsViewController.m
@@ -0,0 +1,51 @@
+//
+//  SettingsViewController.m
+//  iToDo
+//
+//  Created by Bilal ARSLAN on 30/07/14.
+//  Copyright (c) 2014 Bilal ARSLAN. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"Settings";
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
